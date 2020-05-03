9ff0016cb66d5a8be4372639a265595f70a17b10
Alex Grupas <alex.grupas@gmail.com>
2020-05-03 18:08:26 -0500

    working on clock

--- main.c
+++ main.c
@@ -43,7 +43,7 @@ int main(int argc, char** argv) {
     {
         if(wait(NULL))
             break;
-        shmptr->clock_seconds += 100;
+        shmptr->clock_seconds = shmptr->clock_seconds + 100;
     }
 
 

62f4a9d30cea2f65ef959aec925c532accc8a8b8
Alex Grupas <alex.grupas@gmail.com>
2020-05-04 16:35:05 -0500

    debugging

--- user.c
+++ user.c
@@ -34,6 +34,8 @@ int main(int argc, char** argv)
     signal(SIGQUIT, quitSigHandler);
     signal(SIGINT, interruptSigHandler);
 
+
+
     //get shared memory and attach it
     int shmid;
     key_t key = 1234;
@@ -42,6 +44,13 @@ int main(int argc, char** argv)
 
     if ((shmptr = shmat(shmid, NULL, 0)) == (void *) -1)
         quit("user: shmat");
+
+    //remove this
+    if(ID == 2)
+    {
+        shmptr->childID[ID-1] = -1;
+        detach_and_quit();
+    }
 
     //check clock for start time and compute end time
     int clockStartNano = shmptr->clock_nanoseconds;
